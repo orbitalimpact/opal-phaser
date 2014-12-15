f13fde1a375afc77892b598e3f92f8a3e52231ff
Gabriel Rios <gabrielfalcaorios@gmail.com>
2014-12-15 16:28:54 -0300

    Point to phaser cdn

--- demo/main.rb
+++ demo/main.rb
@@ -1,22 +1,28 @@
 module Phaser
+    AUTO = `Phaser.AUTO`
     class Game < `Phaser.Game`
-        Phaser::AUTO = `Phaser.AUTO`
-        
-        def add
-            `this.add`
-        end
+      def initialize(*args)
+        puts "initialize"
+        super
+      end
+
+      def add
+        puts "add"
+      `this.add`
+      end
     end
 end
 
 class Game
     def initialize
-        @game = Phaser::Game.new(800, 600, Phaser::AUTO, "", { :preload => preload, :create => create })
+      puts "Game initialize"
+      @game = Phaser::Game.new(800, 600, Phaser::AUTO, "", { :preload => proc { preload }.to_n, :create => proc { create }.to_n })
     end
-    
+
     def preload
         #@game.load.image("logo", "phaser.png")
     end
-    
+
     def create
         `console.log(this.isBooted)`
         @game.add.text(100, 100, "test")
@@ -24,4 +30,4 @@ class Game
     end
 end
 
-phaser_game = Game.new
+# phaser_game = Game.new
