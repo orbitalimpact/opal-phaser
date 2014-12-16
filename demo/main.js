7870cf507a6aa321355d6a2a9289b75bed682371
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-12-16 16:28:17 -0500

    added a comment explaining main.js

--- demo/main.js
+++ demo/main.js
@@ -1,3 +1,5 @@
+// This file is just a sanity check to see if this demo works in plain JS
+
 function Game () {
     this.game = new Phaser.Game(800, 600, Phaser.AUTO, "", { preload: preload, create: create });
     
