fde18f069e51f0929a88de339d965a4c536457e6
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-12-26 17:43:29 -0500

    made lib/opal/phaser.rb look more like opal-firebase

--- lib/opal/phaser.rb
+++ lib/opal/phaser.rb
@@ -1,8 +1,3 @@
-if RUBY_ENGINE == 'opal'
-  require 'opal/phaser/core'
-else
-  require 'opal'
-  require 'opal/phaser/version'
-
-  Opal.append_path File.expand_path('../..', __FILE__).untaint
-end
+require 'opal'
+require 'opal/phaser/version'
+Opal.append_path File.expand_path('../../../opal', __FILE__).untaint
