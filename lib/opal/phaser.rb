ec1f115c75f847e92e2c93fd8a5089b8b4e7bb90
Gabriel Rios <gabrielfalcaorios@gmail.com>
2014-12-26 21:48:30 -0300

    Revert "made lib/opal/phaser.rb look more like opal-firebase"
    This reverts commit fde18f069e51f0929a88de339d965a4c536457e6.

--- lib/opal/phaser.rb
+++ lib/opal/phaser.rb
@@ -1,3 +1,8 @@
-require 'opal'
-require 'opal/phaser/version'
-Opal.append_path File.expand_path('../../../opal', __FILE__).untaint
+if RUBY_ENGINE == 'opal'
+  require 'opal/phaser/core'
+else
+  require 'opal'
+  require 'opal/phaser/version'
+
+  Opal.append_path File.expand_path('../..', __FILE__).untaint
+end
