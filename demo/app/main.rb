7673587f3d847c18668c6d3c157478ac2c3e9eb9
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-12-27 13:45:04 -0500

    used the `spritesheet` load method, instead of `image`

--- demo/app/main.rb
+++ demo/app/main.rb
@@ -15,7 +15,7 @@ class Game
         game.load.image('sky',     'assets/sky.png')
         game.load.image('ground',  'assets/platform.png')
         game.load.image('star',    'assets/star.png')
-        game.load.image('dude',    'assets/dude.png',    32,  48)
+        game.load.spritesheet('dude',    'assets/dude.png',    32,  48)
       end
 
       state.create do |game|
