4f4b74f1e78914e3a6cbfd9ea35bea27eaea1111
George D. Plymale II <georgedp@orbitalimpact.com>
2014-12-27 21:58:56 -0500

    2nd attempt at making a world class and location change

--- lib/opal/phaser/core/world.rb
+++
@@ -1,8 +0,0 @@
-module Phaser
-    class World
-        def initialize
-            alias_native :x_center, :centerX
-            alias_native :y_center, :centerY
-        end
-    end
-end
---
+++ lib/opal/phaser/world.rb
@@ -0,0 +1,6 @@
+module Phaser
+    module World
+        X_CENTER = %x{ Phaser.World.centerX }
+        Y_CENTER = %x{ Phaser.World.centerY }
+    end
+end
