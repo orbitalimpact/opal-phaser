a9a598dde53c0c36c38a87e80cc60fc640495081
George D. Plymale II <georgedp@orbitalimpact.com>
2015-01-16 15:14:48 -0500

    added an alias for game.staged

--- lib/opal/phaser/core/game.rb
+++ lib/opal/phaser/core/game.rb
@@ -30,6 +30,7 @@ module Phaser
     alias_native :load, :load
     alias_native :add, :add
     alias_native :world, :world
+    alias_native :staged. :staged
     alias_native :physics, :physics
     alias_native :debug, :debug
     alias_native :input, :input
