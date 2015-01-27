27799baf86f79ead3b390468242c916636d7b008
George D. Plymale II <georgedp@orbitalimpact.com>
2015-01-27 12:52:40 -0500

    added some require statements for the new classes

--- lib/opal/phaser/core.rb
+++ lib/opal/phaser/core.rb
@@ -1,3 +1,6 @@
 require 'opal/phaser/core/state'
 require 'opal/phaser/core/game'
 require 'opal/phaser/core/physics'
+require 'opal/phaser/core/game_object_factory'
+require 'opal/phaser/core/sprite'
+require 'opal/phaser/core/group'
