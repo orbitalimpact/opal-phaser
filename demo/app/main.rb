889667dd6fadc53498835692a4e32bfa37e0bae2
George D. Plymale II <georgedp@orbitalimpact.com>
2015-02-18 11:32:00 -0500

    made the body properties work on the sprite

--- demo/app/main.rb
+++ demo/app/main.rb
@@ -73,12 +73,12 @@ class Player
 
     @game.physics.arcade.enable(@player)
 
-    `#@player.body.bounce.y = 0.2`
-    `#@player.body.gravity.y = 300`
-    `#@player.body.collideWorldBounds = true`
+    @player.body.bounce.y = 0.2
+    @player.body.gravity.y = 300
+    @player.body.collideWorldBounds = true
 
-    `#@player.animations.add('left', [0, 1, 2, 3], 10, true)`
-    `#@player.animations.add('right', [5, 6, 7, 8], 10, true)`
+    @player.animations.add('left', [0, 1, 2, 3], 10, true)
+    @player.animations.add('right', [5, 6, 7, 8], 10, true)
   end
 end
 
