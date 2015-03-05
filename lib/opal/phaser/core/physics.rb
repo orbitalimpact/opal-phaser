6d8a1aab6e5fc787baef4443474e399943ab1f1d
George D. Plymale II <georgedp@orbitalimpact.com>
2015-03-05 18:52:40 -0500

    deleted older code for Physics and added newer code

--- lib/opal/phaser/core/physics.rb
+++ lib/opal/phaser/core/physics.rb
@@ -1,35 +1,19 @@
 module Phaser
   class Physics
-  	def initialize(game)
-  		@native = `new Phaser.Physics(game)`
-  	end
+    include Native
+
+    class Arcade
+    	class Body
+    	end
+    end
 
     ARCADE = `Phaser.Physics.ARCADE`
     P2JS   = `Phaser.Physics.P2JS`
     NINJA  = `Phaser.Physics.NINJA`
     BOX2D  = `Phaser.Physics.BOX2D`
 
-    class Arcade
-    	class Body
-        
-        #attr_accessor :bounce
-        #attr_accessor :gravity
-        
-    		def initialize (sprite)
-    			@native = `new Phaser.Physics.Arcade.Body(sprite)`
-          # FUTURE NATIVE METHODS AND PROPERTIES FROM THE BODY CLASS
-          #@bounce = `#@native.bounce`
-          #@gravity = `#@native.gravity`
-          #
-          #def collide_world_bounds= (bool)
-          #  if bool == true
-          #    puts "ran true"
-          #  else
-          #    puts "ran false"
-          #  end
-          #end
-    		end
-    	end
-    end
+    alias_native :start_system, :startSystem
+    alias_native :arcade
+
   end
 end
