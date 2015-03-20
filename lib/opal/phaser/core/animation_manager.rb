999df6d41cedc0c27a5e00d050bfc7c15e18fdef
George D. Plymale II <georgedp@orbitalimpact.com>
2015-03-20 14:34:04 -0400

    revamped Animation class

--- lib/opal/phaser/core/animation_manager.rb
+++ lib/opal/phaser/core/animation_manager.rb
@@ -2,7 +2,7 @@ module Phaser
   class AnimationManager
     include Native
 
-    alias_native :add
+    alias_native :add, :add, as: Animation
     alias_native :stop
     alias_native :play
   end
