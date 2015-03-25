7eb1a6da0815efece256bcbe93c1d4bfaec2c9f9
George D. Plymale II <georgedp@orbitalimpact.com>
2015-03-25 11:02:43 -0400

    added TileSprite class

--- lib/opal/phaser/game_objects/game_object_factory.rb
+++ lib/opal/phaser/game_objects/game_object_factory.rb
@@ -2,11 +2,11 @@ module Phaser
   class GameObjectFactory
     include Native
 
-    alias_native :sprite, :sprite, as: Sprite
-    alias_native :group,  :group,  as: Group
-    alias_native :text,   :text,   as: Text
-    alias_native :image,  :image,  as: Image
-    alias_native :tween,  :tween,  as: Tween
-    alias_native :tile_sprite, :tileSprite
+    alias_native :sprite,      :sprite,     as: Sprite
+    alias_native :group,       :group,      as: Group
+    alias_native :text,        :text,       as: Text
+    alias_native :image,       :image,      as: Image
+    alias_native :tween,       :tween,      as: Tween
+    alias_native :tile_sprite, :tileSprite, as: TileSprite
   end
 end
---
+++ lib/opal/phaser/game_objects/tile_sprite.rb
@@ -0,0 +1,5 @@
+class TileSprite
+  include Native
+  
+  alias_native :auto_scroll, :autoScroll
+end
