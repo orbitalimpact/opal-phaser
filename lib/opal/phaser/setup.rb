29d31a26875bcda548983eef1363b0e82b2a4037
George D. Plymale II <georgedp@orbitalimpact.com>
2015-03-25 19:19:06 -0400

    added tile_sprite require in setup.rb

--- lib/opal/phaser/setup.rb
+++ lib/opal/phaser/setup.rb
@@ -18,6 +18,7 @@ require 'opal/phaser/physics/physics'
 
 require 'opal/phaser/game_objects/events'
 require 'opal/phaser/game_objects/sprite'
+require 'opal/phaser/game_objects/tile_sprite'
 require 'opal/phaser/game_objects/text'
 require 'opal/phaser/game_objects/image'
 require 'opal/phaser/game_objects/game_object_factory'
