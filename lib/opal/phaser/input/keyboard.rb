aafcfe4db4094c3b8f5ccd114877d167ab0043bf
George D. Plymale II <georgedp@orbitalimpact.com>
2015-03-26 17:57:15 -0400

    added SPACEBAR constant in Keyboard class

--- lib/opal/phaser/input/keyboard.rb
+++ lib/opal/phaser/input/keyboard.rb
@@ -2,6 +2,8 @@ module Phaser
   class Keyboard
     include Native
     
+    SPACEBAR = `Phaser.Keyboard.SPACEBAR`
+    
     alias_native :create_cursor_keys, :createCursorKeys
     
     alias_native :add_key, :addKey, as: Key
