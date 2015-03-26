d5c8485b38313fcd696b82dfa9924283572cce02
George Plymale <george@orbitalimpact.com>
2015-03-26 01:47:58 -0400

    Reorder requires to be in correct order

--- lib/opal/phaser/game_objects/files.rb
+++ lib/opal/phaser/game_objects/files.rb
@@ -1,6 +1,7 @@
 # These requires must be loaded in order of dependency:
 require 'opal/phaser/game_objects/events'
 require 'opal/phaser/game_objects/sprite'
+require 'opal/phaser/game_objects/tile_sprite'
 require 'opal/phaser/game_objects/text'
 require 'opal/phaser/game_objects/image'
 require 'opal/phaser/game_objects/game_object_factory'
--- lib/opal/phaser/setup.rb
+++ lib/opal/phaser/setup.rb
@@ -5,10 +5,10 @@ require 'native'
 require 'opal/phaser/core/files'
 require 'opal/phaser/animation/files'
 require 'opal/phaser/tween/files'
+require 'opal/phaser/physics/files'
 require 'opal/phaser/game_objects/files'
 require 'opal/phaser/input/files'
 require 'opal/phaser/loader/files'
-require 'opal/phaser/physics/files'
 
 # TODO: This is odd here as it doesn't fit the files.rb require pattern... how can we clean this up?
 require 'opal/phaser/core/game'
