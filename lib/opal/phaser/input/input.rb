623dd89cfb5ed11a009af308943f6364c6aba502
George D. Plymale II <georgedp@orbitalimpact.com>
2015-04-06 12:42:38 -0400

    added x and y variables / properties for Input

--- lib/opal/phaser/input/input.rb
+++ lib/opal/phaser/input/input.rb
@@ -2,6 +2,9 @@ module Phaser
   class Input
     include Native
     
+    alias_native :x
+    alias_native :y
+    
     alias_native :keyboard, :keyboard, as: Keyboard
     
     alias_native :on_down, :onDown, as: Signal
