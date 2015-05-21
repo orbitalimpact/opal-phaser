e1700a9eed15be43435368f1e80bd18dcc006fec
Gabriel Rios <gabrielfalcaorios@gmail.com>
2015-05-21 15:07:16 -0300

    Alias world setBounds

--- lib/opal/phaser/core/world.rb
+++ lib/opal/phaser/core/world.rb
@@ -1,10 +1,12 @@
 module Phaser
   class World
     include Native
-    
+
     alias_native :width
     alias_native :height
-    
+
+    alias_native :set_bounds, :setBounds
+
     alias_native :x_center, :centerX
     alias_native :y_center, :centerY
     alias_native :random_y, :randomY
