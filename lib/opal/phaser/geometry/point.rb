b045bfcf8e389afdc47120ad9827c754419094be
Gabriel Rios <gabrielfalcaorios@gmail.com>
2015-05-21 15:07:36 -0300

    Point initialize acceps both a native or x and y

--- lib/opal/phaser/geometry/point.rb
+++ lib/opal/phaser/geometry/point.rb
@@ -2,6 +2,16 @@ module Phaser
   class Point
     include Native
 
+    # When its automatically aliased we just pass the object to super
+    def initialize(_x = 0, _y = 0)
+      if `typeof(#{_x})` == "object"
+        super(_x)
+      else
+        @native = `new Phaser.Point(#{_x}, #{_y})`
+        super(@native)
+      end
+    end
+
     alias_native :x
     alias_native :x=
     alias_native :y
