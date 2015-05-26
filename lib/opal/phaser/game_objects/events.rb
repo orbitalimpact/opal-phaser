31a2d28c85c7c091fb3952f4c212dbc179e3516a
Gabriel Rios <gabrielfalcaorios@gmail.com>
2015-05-26 20:15:51 -0300

    Handling events API similar to jQuery

--- lib/opal/phaser/game_objects/events.rb
+++ lib/opal/phaser/game_objects/events.rb
@@ -5,5 +5,18 @@ module Phaser
     include Native
 
     alias_native :on_input_down, :onInputDown, as: Signal
+
+    def on(type, context, &block)
+      case type.to_sym
+      when :up
+        `#@native.onInputUp.add(#{block.to_n}, #{context})`
+      when :down
+        `#@native.onInputDown.add(#{block.to_n}, #{context})`
+      when :out
+        `#@native.onInputOut.add(#{block.to_n}, #{context})`
+      when :over
+        `#@native.onInputOver.add(#{block.to_n}, #{context})`
+      end
+    end
   end
 end
