5fd32959a3b018160d2d7ebecfb2669d616eb81b
ylluminarious <georgedp@orbitalimpact.com>
2015-06-11 14:53:16 -0400

    changed the on method for when there is no block

--- lib/opal/phaser/input/input.rb
+++ lib/opal/phaser/input/input.rb
@@ -1,5 +1,7 @@
 require 'opal/phaser/input/keyboard'
 require 'opal/phaser/input/pointer'
+require 'opal/phaser/core/signal'
+
 module Phaser
   class Input
     include Native
@@ -11,15 +13,19 @@ module Phaser
     alias_native :mouse_pointer,  :mousePointer,  as: Pointer
 
     def on(type, &block)
-      case type.to_sym
-      when :down
-        `#@native.onDown.add(#{block.to_n})`
-      when :up
-        `#@native.onUp.add(#{block.to_n})`
-      when :tap
-        `#@native.onTap.add(#{block.to_n})`
-      when :hold
-        `#@native.onHold.add(#{block.to_n})`
+      if block_given?
+        case type.to_sym
+        when :down
+          `#@native.onDown.add(#{block.to_n})`
+        when :up
+          `#@native.onUp.add(#{block.to_n})`
+        when :tap
+          `#@native.onTap.add(#{block.to_n})`
+        when :hold
+          `#@native.onHold.add(#{block.to_n})`
+        end
+      else
+        Signal.new
       end
     end
     alias_native :active_pointer, :activePointer
