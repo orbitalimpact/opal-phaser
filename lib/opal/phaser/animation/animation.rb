f23eb342260459e4924a83f560909ba8310ca620
George D. Plymale II <georgedp@orbitalimpact.com>
2015-06-29 19:06:23 -0400

    added Phaser::Animation#generate_frame_names

--- lib/opal/phaser/animation/animation.rb
+++ lib/opal/phaser/animation/animation.rb
@@ -15,6 +15,16 @@ module Phaser
       `#@native.loop = bool`
     end
 
+    def self.generate_frame_names(args = {})
+      prefix        = args[:prefix]
+      start_num     = args[:start_num]
+      stop_num      = args[:stop_num]
+      suffix        = args[:suffix, ""]
+      zeros_padding = args[:zeros_padding, 0]
+      
+      `Phaser.Animation.generateFrameNames(prefix, start_num, stop_num, suffix, zeros_padding)`
+    end
+
     alias_native :is_playing?, :isPlaying
   end
 end
