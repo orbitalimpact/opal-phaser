6eb6cb13f39f84d4932bb6d218290ed7e9b0a869
George D. Plymale II <georgedp@orbitalimpact.com>
2015-07-02 17:39:18 -0400

    added an Easing module for tweening

---
+++ lib/opal/phaser/tween/easing.rb
@@ -0,0 +1,17 @@
+module Phaser
+  module Easing
+    module Quadratic
+      def self.In
+        `Phaser.Easing.Quadratic.In`
+      end
+      
+      def self.InOut
+        `Phaser.Easing.Quadratic.InOut`
+      end
+      
+      def self.Out
+        `Phaser.Easing.Quadratic.Out`
+      end
+    end
+  end
+end
--- lib/opal/phaser/tween/files.rb
+++ lib/opal/phaser/tween/files.rb
@@ -1,2 +1,3 @@
 # These requires must be loaded in order of dependency:
 require 'opal/phaser/tween/tween'
+require 'opal/phaser/tween/easing'
