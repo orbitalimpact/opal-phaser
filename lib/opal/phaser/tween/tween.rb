3bcf8d57b498b9b43bb87102c0584e668e3e2c54
Gabriel Rios <gabrielfalcaorios@gmail.com>
2015-07-23 16:04:51 -0300

    Tween events

--- lib/opal/phaser/tween/tween.rb
+++ lib/opal/phaser/tween/tween.rb
@@ -1,8 +1,23 @@
 module Phaser
   class Tween
     include Native
-    
-    alias_native :to
+
+    alias_native :to, :to, as: Tween
     alias_native :start
+
+    def on(type, context = nil, &block)
+      case type.to_sym
+      when :child_complete
+        `#@native.onChildComplete.add(#{block.to_n}, #{context})`
+      when :complete
+        `#@native.onComplete.add(#{block.to_n}, #{context})`
+      when :loop
+        `#@native.onLoop.add(#{block.to_n}, #{context})`
+      when :repeat
+        `#@native.onRepeat.add(#{block.to_n}, #{context})`
+      when start
+        `#@native.onStart.add(#{block.to_n}, #{context})`
+      end
+    end
   end
 end
