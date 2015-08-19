0eebabb8aedd6c6b90a3309374ea200cc328ea99
George D. Plymale II <georgedp@orbitalimpact.com>
2015-08-19 19:37:23 -0400

    add some code to Phaser::Image for handling input

--- lib/opal/phaser/game_objects/image.rb
+++ lib/opal/phaser/game_objects/image.rb
@@ -1,4 +1,5 @@
 require 'opal/phaser/core/anchor'
+require 'opal/phaser/game_objects/events'
 
 module Phaser
   class Image < PIXI::Sprite
@@ -10,6 +11,11 @@ module Phaser
 
     alias_native :smoothed=
 
+    def input_enabled=(bool)
+      `#@native.inputEnabled = bool`
+    end
+
     alias_native :load_texture, :loadTexture
+    alias_native :events,       :events,    as: Events
   end
 end
