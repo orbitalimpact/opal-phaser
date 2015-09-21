f7077a5d5ed3f79fe5c6de20c9851ccf9985b0e9
George D. Plymale II <georgedp@orbitalimpact.com>
2015-09-21 14:37:18 -0400

    refactor Phaser::Tween#to so that it accepts an arg hash

--- lib/opal/phaser/tween/tween.rb
+++ lib/opal/phaser/tween/tween.rb
@@ -2,8 +2,19 @@ module Phaser
   class Tween
     include Native
 
-    alias_native :to, :to, as: Tween
     alias_native :start
+
+    def to(args = {})
+      properties = args[:properties].to_n
+      duration   = args[:duration, 1000]
+      ease       = args[:ease, nil]
+      auto_start = args[:auto_start, false]
+      delay      = args[:delay, 0]
+      repeat     = args[:repeat, 0]
+      yoyo       = args[:yoyo, false]
+      
+      `#@native.to(properties, duration, ease, auto_start, delay, repeat, yoyo)`
+    end
 
     def on(type, context = nil, &block)
       case type.to_sym
