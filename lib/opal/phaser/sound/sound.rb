696187f147ea63afb7e75e16313945b645525e5e
George D. Plymale II <georgedp@orbitalimpact.com>
2015-09-28 11:43:58 -0400

    make Phaser::Sound#play have an argument hash

--- lib/opal/phaser/sound/sound.rb
+++ lib/opal/phaser/sound/sound.rb
@@ -2,7 +2,18 @@ module Phaser
   class Sound
     include Native
     
-    alias_native :play
+    def play(args = {})
+      arg_list = {marker: "", position: 0, volume: 1, loop: false, force_restart: true}
+
+      arg_list.each do |name, default_value|
+        unless args.include?(name)
+          args[name] = default_value
+        end
+      end
+      
+      `#@native.play(#{args[:marker]}, #{args[:position]}, #{args[:volume]}, #{args[:loop]}, #{args[:force_restart]})`
+    end
+    
     alias_native :stop
   end
 end
