ef6a04631ef460337686d60e6d827480d554e470
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-06-20 21:29:02 +0100

    Remaining APIs I needed

--- lib/opal/phaser/game_objects/button.rb
+++ lib/opal/phaser/game_objects/button.rb
@@ -1,7 +1,9 @@
+require 'opal/phaser/geometry/point'
 module Phaser
   class Button
     include Native
-    
+
+    alias_native :anchor, :anchor, as: Point
     alias_native :load_texture, :loadTexture
   end
 end
--- lib/opal/phaser/game_objects/text.rb
+++ lib/opal/phaser/game_objects/text.rb
@@ -1,11 +1,16 @@
+require 'opal/phaser/geometry/point'
 module Phaser
   class Text
     include Native
-    
+
     alias_native :destroy
-    
-    def text=(text)
-      `#@native.text = text`
-    end
+    alias_native :anchor, :anchor, as: Point
+
+    native_accessor :x
+    native_accessor :y
+    native_accessor :angle
+    native_accessor :fill
+    native_accessor :visible
+    native_accessor :text
   end
 end
